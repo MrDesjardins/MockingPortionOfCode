9ffee7f09c728d80d17c5e35a7b7e4ccd940f945
Patrick Desjardins <padesjar@microsoft.com>
2014-11-14 08:24:48 -0800

    Small Example Mocking Portion of Tested Interface
    This code demonstrate how to Mock something that we are testing. For
    example, we have a single class that inherit from an interface and we
    want to test one method of that interface whilst using the concrete
    implementation of the second method.

---
+++ MockingPortionOfCode/ClassesToTest/MyConcreteImplementation.cs
@@ -0,0 +1,25 @@
+﻿namespace MockingPortionOfCode.ClassesToTest
+{
+    using System.Threading;
+
+    public class MyConcreteImplementation : IMyInterface
+    {
+        public MyConcreteImplementation(int counter)
+        {
+            Counter = counter;
+        }
+
+        public int Counter { get; set; }
+        public void TransformationA()
+        {
+            Counter++;
+            TransformationB();
+            Counter++;
+        }
+
+        public void TransformationB()
+        {
+            Thread.Sleep(25000);
+        }
+    }
+}
---
+++ MockingPortionOfCode/Tests/TransformationATest.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace MockingPortionOfCode
+{
+    using ClassesToTest;
+    using Moq;
+
+    [TestClass]
+    public class TransformationATest
+    {
+        [TestMethod]
+        public void MyTest()
+        {
+            //Arrange
+            var whatWeWantToTest = new MyConcreteImplementation(100);
+            var mock = new Mock<IMyInterface>();
+            mock.Setup(d => d.TransformationA()).Callback(whatWeWantToTest.TransformationA);
+            // -- We do nothing for B --
+
+            // Act
+            mock.Object.TransformationA();
+
+            //Assert
+            Assert.AreEqual(102, whatWeWantToTest.Counter);
+
+        }
+    }
+}
